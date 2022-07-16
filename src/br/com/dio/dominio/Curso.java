7efc4bf370748019823715e0c979c081783c2cca
Vinicius Marques <vinicius.marques54@gmail.com>
2022-07-16 20:16:49 -0300

    Changes

---
+++ src/br/com/dio/dominio/Curso.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.dio.dominio;
+
+/**
+ *
+ * @author vinic
+ */
+public class Curso extends Conteudo{
+    
+    private int cargaHoraria;
+    
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + '}';
+    }
+    
+}
--- src/main.java
+++ src/main.java
@@ -1,3 +1,10 @@
+
+import br.com.dio.dominio.Bootcamp;
+import br.com.dio.dominio.Curso;
+import br.com.dio.dominio.Dev;
+import br.com.dio.dominio.Mentoria;
+import java.time.LocalDate;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,7 +20,56 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+        
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+        
+        /*System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);*/
+        
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+        
+        Dev jose = new Dev();
+        jose.setNome("José");
+        jose.inscreverBootcamp(bootcamp);
+        System.out.println("Conteídos Inscritos Jose: " + jose.getConteudosInscritos());
+        
+        jose.progredir();
+        jose.progredir();
+        System.out.println("Conteídos Inscritos Jose: " + jose.getConteudosInscritos());
+        System.out.println("Conteídos Concluidos Jose: " + jose.getConteudosConcluidos());
+        System.out.println("XP: "  + jose.calcularTotalXP());
+        
+        System.out.println("--------------------------------------------------------------------");
+        
+        Dev joao = new Dev();
+        joao.setNome("João");
+        joao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteídos Inscritos Joao: " + joao.getConteudosInscritos());
+   
+        joao.progredir();
+        joao.progredir();
+        joao.progredir();
+        System.out.println("Conteídos Inscritos Joao: " + joao.getConteudosInscritos());
+        System.out.println("Conteídos Concluidos Joao: " + joao.getConteudosConcluidos());
+        System.out.println("XP: "  + joao.calcularTotalXP());
     }
     
 }
